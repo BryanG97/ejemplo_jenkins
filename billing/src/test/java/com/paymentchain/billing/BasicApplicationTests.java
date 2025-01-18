d24bac636d03fc4caf552b731bd2ba3df8be3e51
Bryan Guevara <bguevara@ec.krugercorp.com>
2025-01-18 13:42:37 -0500

    Prueba cambio

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 7";
+       String message = "Default Message Cambio 5";
         assertNotNull(message);
     }
 }
